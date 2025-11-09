e28e173c76bc7c62d2a0a9d545189b212426a586
pluaj <plua.juan@fisagrp>
2025-11-09 06:26:11 -0500

    deploy main integracion

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,28 @@
+'use client';
+import { Button } from "@/components/ui/button";
 import { UsersDataTable } from "@/components/users-data-table"
 import { Users } from "lucide-react"
-
+import { use, useState } from "react";
 export default function Home() {
+const [respuesta, setRespuesta] = useState<string>("");
+ const llamarJava = async () => {
+    try {
+      const res = await fetch("https://route-shadcn-data-table-clone-pluaj-dev-dev.apps.rm3.7wse.p1.openshiftapps.com/");
+      if (!res.ok) throw new Error(`HTTP ${res.status}`);
+      const text = await res.text();
+      setRespuesta(text);
+    } catch (err) {
+      setRespuesta("Error: " + String(err));
+    }
+  };
   return (
     <main className="min-h-screen bg-gradient-to-br from-background via-background to-secondary/20">
       <div className="container mx-auto px-4 py-12">
+         <div>
+        <Button onClick={llamarJava} className="btn">Llamar Java</Button>
+        <pre>{respuesta}</pre>
+      </div>
+
         <div className="mb-10">
           <div className="flex items-center gap-3 mb-3">
             <div className="flex h-12 w-12 items-center justify-center rounded-xl bg-primary/10">
