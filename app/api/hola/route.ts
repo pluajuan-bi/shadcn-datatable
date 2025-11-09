1cb4ebd8ae23e44a89935e6c3af471f90812b226
pluaj <plua.juan@fisagrp>
2025-11-09 18:32:24 -0500

    deploy main helm

--- app/api/hola/route.ts
+++ app/api/hola/route.ts
@@ -1,7 +1,7 @@
 export async function GET() {
   try {
     // Internal service address inside the OpenShift cluster
-    const backendUrl = "http://service-microservicio-1.pluaj-dev-dev.svc.cluster.local:8082/";
+    const backendUrl = "http://service-microservicio-2.pluaj-dev-dev.svc.cluster.local:8082/";
     
     const res = await fetch(backendUrl, { cache: "no-store" });
     const text = await res.text();
